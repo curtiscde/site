f7e498aa817cb7584f1a58abb7361151fed713cc
curtiscde <curt@live.co.uk>
2024-12-10 17:49:03 +0000

    ts-expect-error

--- src/app/posts/parsePost.ts
+++ src/app/posts/parsePost.ts
@@ -1,5 +1,6 @@
 import { Post } from '../types/Post';
 
+// @ts-expect-error any for now
 export const parsePost = ({ date, description, id, title, slug, tags, image }: any, content: string): Post => {
   const post: Post = {
     content,
