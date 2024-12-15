064de54fd292426d53cc2306a04ded80a2b5fb7e
curtiscde <curt@live.co.uk>
2024-12-15 22:17:21 +0000

    rename

--- src/app/components/PostCard.tsx
+++ src/app/components/PostCard.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Post } from "../types/Post"
-import "./PostCard.scss"
+import "./postcard.scss"
 
 export const PostCard = ({ data: post }: { index?: number, data: Post, width?: number }) => {
   const today = new Date();
