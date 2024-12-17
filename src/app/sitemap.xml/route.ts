b2c4e2ad3eb490d6320c2f82f91c1bc11be9445b
curtiscde <curt@live.co.uk>
2024-12-17 17:27:25 +0000

    update sitemap and add to footer

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,26 +1,33 @@
-import { getPosts } from '../util/posts'
+import { filterPostsByTag, getPosts, getTopTags } from '../util/posts'
 import { config } from '../config'
 
 export const dynamic = 'force-static'
 
 const generateSitemap = async () => {
   const posts = getPosts()
+  const tags = getTopTags(posts)
 
   return `
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-<url>
-<loc>https://www.curtiscode.dev/</loc>
-<lastmod>2024-12-07T12:00:00+00:00</lastmod>
-</url>
-${posts.map(post => {
-    return `
   <url>
-<loc>${config.url}/post/${post.slug}</loc>
-<lastmod>${post.date}</lastmod>
-</url>
-
-  `
-  })}
+  <loc>https://www.curtiscode.dev/</loc>
+  <lastmod>2024-12-07T12:00:00+00:00</lastmod>
+  </url>
+  ${posts.map(post => {
+    return `<url>
+    <loc>${config.url}/post/${post.slug}</loc>
+    <lastmod>${post.date.toISOString()}</lastmod>
+  </url>
+  `}).join('')}
+  ${tags.map(({ tag }) => {
+      const tagPosts = filterPostsByTag(posts, tag)
+      const mostRecentPost = tagPosts.sort((postA, postB) => postB.date.getTime() - postA.date.getTime())[0]
+      return `
+  <url>
+    <loc>${config.url}/tag/${tag}</loc>
+    <lastmod>${mostRecentPost.date.toISOString()}</lastmod>
+  </url>`
+    }).join('')}
 </urlset>`
 }
 
