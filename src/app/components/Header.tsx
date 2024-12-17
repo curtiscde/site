ecdc3747b1c6a8a0da67dcc396c6e68e84fc2bc3
curtiscde <curt@live.co.uk>
2024-12-17 19:14:58 +0000

    add header positioning

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 export const Header = () => (
   <div className="navbar bg-base-100">
-    <div className="avatar mx-2">
-      <div className="w-10 rounded-full">
-        <img src="/images/curtis.jpeg" />
+    <div className="container mx-auto">
+      <div className="avatar mx-2">
+        <div className="w-10 rounded-full">
+          <img src="/images/curtis.jpeg" />
+        </div>
       </div>
+      <Link href="/" className="btn btn-ghost text-xl">Curtis Timson</Link>
     </div>
-    <Link href="/" className="btn btn-ghost text-xl">Curtis Timson</Link>
   </div>
 )
