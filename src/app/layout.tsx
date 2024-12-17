b833c6daee2cc566ea6b4645ac689d274901b6b2
curtiscde <curt@live.co.uk>
2024-12-17 18:07:53 +0000

    add rss to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { config } from './config'
 
 export const metadata: Metadata = {
   title: config.pageTitle,
+  icons: '//0.gravatar.com/avatar/15a556c8b58e8aefa727088885925a12.png?s=16',
+  alternates: { types: { "application/rss+xml": `${config.url}/rss.xml`, }, }
 };
 
 export default async function RootLayout({
