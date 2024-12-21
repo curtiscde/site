a18bd4a7417366945c5e7b567187de15bd07a1c4
curtiscde <curt@live.co.uk>
2024-12-21 15:26:50 +0000

    add linkedin

--- src/app/components/Footer/SocialLinks.tsx
+++ src/app/components/Footer/SocialLinks.tsx
@@ -1,7 +1,7 @@
 import { config } from "@/app/config";
 
 export default function SocialLinks() {
-  const { bluesky, stackoverflow, github, x, rss } = config.socialLinks;
+  const { bluesky, stackoverflow, github, x, linkedin, rss } = config.socialLinks;
 
   return (
     <div className="grid grid-flow-col gap-4">
@@ -39,6 +39,15 @@ export default function SocialLinks() {
           viewBox="0 0 24 24"
           className="fill-current">
           <path d="M 2.8671875 3 L 9.7363281 12.818359 L 2.734375 21 L 5.3808594 21 L 10.919922 14.509766 L 15.460938 21 L 21.371094 21 L 14.173828 10.697266 L 20.744141 3 L 18.138672 3 L 12.996094 9.0097656 L 8.7988281 3 L 2.8671875 3 z"></path>
+        </svg>
+      </a>
+      <a href={linkedin}>
+        <svg xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 50 50"
+          className="fill-current">
+          <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
         </svg>
       </a>
       <a href={rss}>
