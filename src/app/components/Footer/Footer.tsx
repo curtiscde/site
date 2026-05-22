48f6507b3b53f94df79334075872b0b1ad338b4e
Curtis Timson <curt@live.co.uk>
2026-05-22 17:33:23 +0100

    feat: Update Ko-Fi SVG

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ import Image from "next/image"
 import "./Footer.scss"
 import { config } from "../../config"
 import SocialLinks from "./SocialLinks"
-import { Laptop, FileText, Cookie, Coffee, Map } from "lucide-react"
+import { Laptop, FileText, Cookie, Map } from "lucide-react"
 
 export const Footer = ({ recentPosts, topTags }: { recentPosts: Post[], topTags: TagCount[] }) => {
   const tagsToDisplay = 12
@@ -55,7 +55,7 @@ export const Footer = ({ recentPosts, topTags }: { recentPosts: Post[], topTags:
               <Link className="link link-hover flex items-center gap-2" href="/uses"><Laptop size={16} />Uses</Link>
               <Link className="link link-hover flex items-center gap-2" href="/privacy-policy"><FileText size={16} />Privacy policy</Link>
               <button className="link link-hover text-left flex items-center gap-2" onClick={() => { localStorage.removeItem('cookie-consent'); window.location.reload() }}><Cookie size={16} />Cookie settings</button>
-              <a className="flex items-center gap-2" href="https://ko-fi.com/curtiscode"><Coffee size={16} />Support me on Ko-fi</a>
+              <a className="flex items-center gap-2" href="https://ko-fi.com/curtiscode"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32" fill="currentColor"><path d="M31.844 11.932c-1.032-5.448-6.48-6.125-6.48-6.125h-24.4c-0.808 0-0.907 1.063-0.907 1.063s-0.109 9.767-0.027 15.767c0.22 3.228 3.448 3.561 3.448 3.561s11.021-0.031 15.953-0.067c3.251-0.568 3.579-3.423 3.541-4.98 5.808 0.323 9.896-3.776 8.871-9.219zM17.093 16.615c-1.661 1.932-5.348 5.297-5.348 5.297s-0.161 0.161-0.417 0.031c-0.099-0.073-0.14-0.12-0.14-0.12-0.595-0.588-4.491-4.063-5.381-5.271-0.943-1.287-1.385-3.599-0.119-4.948 1.265-1.344 4.005-1.448 5.817 0.541 0 0 2.083-2.375 4.625-1.281 2.536 1.095 2.443 4.016 0.963 5.751zM25.323 17.251c-1.24 0.156-2.244 0.036-2.244 0.036v-7.573h2.359c0 0 2.631 0.735 2.631 3.516 0 2.552-1.313 3.557-2.745 4.021z" /></svg>Support me on Ko-fi</a>
               <a className="link link-hover flex items-center gap-2" href="/sitemap.xml"><Map size={16} />Sitemap.xml</a>
             </nav>
           </footer>
